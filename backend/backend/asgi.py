aa759dfd166e688b746a1b06c68f91c6c4475888
rqian <605557526@qq.com>
2023-08-26 01:26:16 +1200

    🐛 Asgi

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -8,9 +8,17 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
-
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from . import routings
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
 
-application = get_asgi_application()
+django_asgi_app = get_asgi_application()
+
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+        "websocket": URLRouter(routings.websocket_urlpatterns),
+    }
+)
