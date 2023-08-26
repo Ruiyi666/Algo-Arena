eaafc3834c1d781154198cd901b9264e237cfe70
rqian <605557526@qq.com>
2023-08-26 21:19:41 +1200

    🐛 It works

---
+++ frontend/src/components/Tile.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="border border-gray-300 aspect-square text-center">
+        <slot></slot>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    color: {
+        type: Number,
+        default: 0,
+    }
+})
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
